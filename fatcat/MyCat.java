396bef65a188ff0a7e39a17bf10b724f1d945c0d
EstebanZG999 <zam22119@uvg.edu.gt>
2022-08-05 22:38:23 -0600

    Iniciando proyecto FatCat
    Se sube todo el proyecto FatCat

---
+++ fatcat/MyCat.java
@@ -0,0 +1,47 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * MyCat is your own cat. Get it to do things by writing code in its act method.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyCat extends Cat
+{
+    /**
+     * Act - do whatever the MyCat wants to do.
+     */
+    public void act()
+    {
+        if (isHungry() == true){
+            eat();
+        }
+        if (isSleepy() == true){
+            sleep(3);
+            shoutHooray();
+        }else{
+            shoutHooray();
+        }
+        if (isBored() == true){
+            dance();
+        }
+        if (isAlone() == true){
+            sleep(4);
+        }
+        if (hasCompany() == true){
+            shoutHooray();
+        }
+        
+        /**
+         * eat();
+        dance();
+        sleep(2);
+        eat();
+        walkLeft(3);
+        walkRight(4);
+        shoutHooray();
+        wait(3);
+        walkLeft(1);
+        */
+    }    
+}
